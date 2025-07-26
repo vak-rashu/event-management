73bf395ef4b010634eaa8c7e52bbaf3571232d6f
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-07-26 11:56:03 +0530

    feat: QR Code for ticket

--- events/ticketing/doctype/event_ticket/event_ticket.py
+++ events/ticketing/doctype/event_ticket/event_ticket.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, BWH Studios and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -18,7 +18,32 @@ class EventTicket(Document):
 		attendee_name: DF.Data
 		booking: DF.Link | None
 		event: DF.Link
+		qr_code: DF.AttachImage | None
 		ticket_type: DF.Link
 	# end: auto-generated types
 
-	pass
+	def before_submit(self):
+		self.generate_qr_code()
+
+	def generate_qr_code(self):
+		import io
+
+		import qrcode
+
+		img = qrcode.make(f"{self.name}")
+		output = io.BytesIO()
+		img.save(output, format="PNG")
+		hex_data = output.getvalue()
+
+		qr_code_file = frappe.get_doc(
+			{
+				"doctype": "File",
+				"content": hex_data,
+				"attached_to_doctype": "Event Ticket",
+				"attached_to_name": self.name,
+				"attached_to_field": "qr_code",
+				"file_name": f"ticket-qr-code-{self.name}.png",
+			}
+		).save()
+
+		self.qr_code = qr_code_file.file_url
