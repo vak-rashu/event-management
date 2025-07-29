eca5aeccfc7653ad62eb44feb902b2e166590d45
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-07-29 19:32:15 +0530

    feat(UX): Publish/Unpublish using button
    Closes #7

--- events/events/doctype/fe_event/fe_event.js
+++ events/events/doctype/fe_event/fe_event.js
@@ -6,5 +6,11 @@ frappe.ui.form.on("FE Event", {
 		frappe.call("frappe.geo.country_info.get_country_timezone_info").then(({ message }) => {
 			frm.fields_dict.time_zone.set_data(message.all_timezones);
 		});
+
+		const button_label = frm.doc.is_published ? __("Unpublish") : __("Publish");
+		frm.add_custom_button(button_label, () => {
+			frm.set_value("is_published", !frm.doc.is_published);
+			frm.save();
+		});
 	},
 });
