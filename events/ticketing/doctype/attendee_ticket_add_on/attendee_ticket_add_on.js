5c580c37e39b3b060b7c32e64f792136a0e76cd2
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-08-02 11:59:49 +0530

    feat: show values for add-ons dynamically
    Closes #9

--- events/ticketing/doctype/attendee_ticket_add_on/attendee_ticket_add_on.js
+++ events/ticketing/doctype/attendee_ticket_add_on/attendee_ticket_add_on.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Attendee Ticket Add-on", {
-// 	refresh(frm) {
-
-// 	},
 // });
+
+frappe.ui.form.on("Ticket Add-on Value", {
+	add_on(frm, cdt, cdn) {
+		const doc = frappe.get_doc(cdt, cdn);
+		frappe.db.get_value("Ticket Add-on", doc.add_on, "options").then(({ message }) => {
+			if (message.options) {
+				const options = message.options.trim().split("\n");
+				frm.grids[0].grid.grid_rows_by_docname[cdn].on_grid_fields_dict.value.set_data(
+					options
+				);
+			}
+		});
+	},
+});
